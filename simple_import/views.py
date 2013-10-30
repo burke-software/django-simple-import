burke-software/django-simple-import

commit a9a6b4e087caae42250586c8806e3d5b353aecd7

Fixed broken test, added new test, added more assertions. Tests no longer assume app lives at /simple_import/.

simple_import/views.py
@@ -9,7 +9,7 @@ from django.db.models import Q, ForeignKey
 from django.db import transaction
 from django.db import IntegrityError
 from django.core.exceptions import ObjectDoesNotExist
-from django.forms.models import modelformset_factory
+from django.forms.models import inlineformset_factory
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
@@ -55,17 +55,22 @@ def get_custom_fields_from_model(model_class):
             content_type = None
         custom_fields = CustomField.objects.filter(content_type=content_type)
         return custom_fields
-    
-    
+
+
 @staff_member_required
 def match_columns(request, import_log_id):
     """ View to match import spreadsheet columns with database fields
     """
     import_log = get_object_or_404(ImportLog, id=import_log_id)
+    
     if not request.user.is_superuser and import_log.user != request.user:
         raise SuspiciousOperation("Non superuser attempting to view other users import")
     
-    MatchFormSet = modelformset_factory(ColumnMatch, form=MatchForm, extra=0)
+    # need to generate matches if they don't exist already
+    import_log.get_matches()
+    
+    MatchFormSet = inlineformset_factory(ImportSetting, ColumnMatch, form=MatchForm, extra=0)
+    
     import_data = import_log.get_import_file_as_list()
     header_row = [x.lower() for x in import_data[0]] # make all lower 
     sample_row = import_data[1]
@@ -75,16 +80,19 @@ def match_columns(request, import_log_id):
     field_names = model_class._meta.get_all_field_names()
     
     if request.method == 'POST':
-        formset = MatchFormSet(request.POST)
+        formset = MatchFormSet(request.POST, instance=import_log.import_setting)
         if formset.is_valid():
             formset.save()
             if import_log.import_type in ["U", "O"]:
-                if 'update_key' in request.POST and request.POST['update_key']:
-                    field_name = import_log.import_setting.columnmatch_set.get(column_name=request.POST['update_key']).field_name
+                update_key = request.POST.get('update_key', '')
+                
+                if update_key:
+                    field_name = import_log.import_setting.columnmatch_set.get(column_name=update_key).field_name
                     if field_name:
                         field_object, model, direct, m2m = model_class._meta.get_field_by_name(field_name)
+                        
                         if direct and field_object.unique:
-                            import_log.update_key = request.POST['update_key']
+                            import_log.update_key = update_key
                             import_log.save()
                         else:
                             errors += ['Update key must be unique. Please select a unique field.']
@@ -96,6 +104,7 @@ def match_columns(request, import_log_id):
                 import_log,
                 model_class,
                 header_row)
+            
             all_field_names = []
             for clean_data in formset.cleaned_data:
                 if clean_data['field_name']:
@@ -107,8 +116,7 @@ def match_columns(request, import_log_id):
                     match_relations,
                     kwargs={'import_log_id': import_log.id}))
     else:
-        existing_matches = import_log.get_matches()
-        formset = MatchFormSet(queryset=existing_matches)
+        formset = MatchFormSet(instance=import_log.import_setting)
     
     field_choices = (('', 'Do Not Use'),)
     for field_name in field_names:
@@ -155,7 +163,7 @@ def match_columns(request, import_log_id):
     
     return render_to_response(
         'simple_import/match_columns.html',
-        {'import_log': import_log, 'formset':formset, 'errors': errors},
+        {'import_log': import_log, 'formset': formset, 'errors': errors},
         RequestContext(request, {}),)
 
 
@@ -177,44 +185,53 @@ def match_relations(request, import_log_id):
     matches = import_log.get_matches()
     field_names = []
     choice_set = []
+    
     for match in matches.exclude(field_name=""):
-        if (not match.field_name.startswith('simple_import_custom__') and
-            not match.field_name.startswith('simple_import_method__')):
-            field, model, direct, m2m = model_class._meta.get_field_by_name(match.field_name)
+        field_name = match.field_name
+        
+        if not field_name.startswith('simple_import_custom__') and \
+                not field_name.startswith('simple_import_method__'):
+            field, model, direct, m2m = model_class._meta.get_field_by_name(field_name)
+            
             if m2m or isinstance(field, ForeignKey): 
                 RelationalMatch.objects.get_or_create(
                     import_log=import_log,
-                    field_name=match.field_name)
-                field_names += [match.field_name]
+                    field_name=field_name)
+                
+                field_names.append(field_name)
                 choices = ()
                 for field in get_direct_fields_from_model(field.related.parent_model()):
                     if field.unique:
-                        choices += ((field.name, field.verbose_name),)
+                        choices += ((field.name, unicode(field.verbose_name)),)
                 choice_set += [choices]
     
-    existing_matches = RelationalMatch.objects.filter(
-        import_log=import_log,
-        field_name__in=field_names)
-    MatchRelationFormSet = modelformset_factory(
+    existing_matches = import_log.relationalmatch_set.filter(field_name__in=field_names)
+    
+    MatchRelationFormSet = inlineformset_factory(
+        ImportLog,
         RelationalMatch,
         form=MatchRelationForm, extra=0)
+    
     if request.method == 'POST':
-        formset = MatchRelationFormSet(request.POST)
+        formset = MatchRelationFormSet(request.POST, instance=import_log)
+        
         if formset.is_valid():
             formset.save()
-            get = ''
+            
+            url = reverse('simple_import-do_import',
+                kwargs={'import_log_id': import_log.id})
+            
             if 'commit' in request.POST:
-                get = "?commit=True"
-            return HttpResponseRedirect(reverse(
-                do_import,
-                kwargs={'import_log_id': import_log.id}) + get)
+                url += "?commit=True"
+            
+            return HttpResponseRedirect(url)
     else:
-        formset = MatchRelationFormSet(queryset=existing_matches)
+        formset = MatchRelationFormSet(instance=import_log)
     
     for i, form in enumerate(formset.forms):
         choices = choice_set[i]
         form.fields['related_field_name'].widget = forms.Select(choices=choices)
-        
+    
     return render_to_response(
         'simple_import/match_relations.html',
         {'formset': formset,
@@ -430,3 +447,4 @@ def start_import(request):
             Q(permission__user=request.user, permission__codename__startswith="change_")).distinct()
     
     return render_to_response('simple_import/import.html', {'form':form,}, RequestContext(request, {}),)
+

simple_import/models.py
@@ -14,8 +14,8 @@ class ImportSetting(models.Model):
     
     class Meta():
         unique_together = ('user', 'content_type',)
-    
-    
+
+
 class ColumnMatch(models.Model):
     """ Match column names from the user uploaded file to the database """
     column_name = models.CharField(max_length=200)
@@ -52,9 +52,8 @@ class ColumnMatch(models.Model):
             if hasattr(field, 'verbose_name'):
                 if field.verbose_name.lower().replace(' ', '_') == normalized_field_name:
                     self.field_name = field_name
-            
 
-    
+
 class ImportLog(models.Model):
     """ A log of all import attempts """
     name = models.CharField(max_length=255)
@@ -93,24 +92,29 @@ class ImportLog(models.Model):
         """ Get each matching header row to database match
         Returns a ColumnMatch queryset"""
         header_row = self.get_import_file_as_list(only_header=True)
-        matches = ColumnMatch.objects.none()
         match_ids = []
+        
         for i, cell in enumerate(header_row):
-            if cell: # Sometimes we get blank headers, ignore them.
-                try:
-                    match = ColumnMatch.objects.get(
-                        import_setting = self.import_setting,
-                        column_name = cell,
-                    )
-                except ColumnMatch.DoesNotExist:
-                    match = ColumnMatch(
-                        import_setting = self.import_setting,
-                        column_name = cell,
-                    )
-                    match.guess_field()
-                match.header_position = i
-                match.save()
-                match_ids += [match.id]
+            if not cell: # Sometimes we get blank headers, ignore them.
+                continue
+            
+            try:
+                match = ColumnMatch.objects.get(
+                    import_setting = self.import_setting,
+                    column_name = cell,
+                )
+            except ColumnMatch.DoesNotExist:
+                match = ColumnMatch(
+                    import_setting = self.import_setting,
+                    column_name = cell,
+                )
+                match.guess_field()
+            
+            match.header_position = i
+            match.save()
+            
+            match_ids += [match.id]
+        
         return ColumnMatch.objects.filter(pk__in=match_ids).order_by('header_position')
 
     def get_import_file_as_list(self, only_header=False):
@@ -203,9 +207,10 @@ class RelationalMatch(models.Model):
     field_name = models.CharField(max_length=255) # Ex student_number_set
     related_field_name = models.CharField(max_length=255, blank=True) # Ex username
 
-    
+
 class ImportedObject(models.Model):
     import_log = models.ForeignKey(ImportLog)
     object_id = models.IntegerField()
     content_type = models.ForeignKey(ContentType)
     content_object = generic.GenericForeignKey('content_type', 'object_id')
+

simple_import/templates/simple_import/match_columns.html
@@ -1,4 +1,4 @@
-<h1>Match columns</h1>
+<h1>Match Columns</h1>
 <form action="" method="post">{% csrf_token %}
     {{ formset.management_form }}
     

simple_import/tests.py
@@ -1,8 +1,8 @@
 import os
 
 from django.contrib.contenttypes.models import ContentType
+from django.core.urlresolvers import reverse
 from django.test import TestCase
-from django.test.client import Client
 from simple_import.compat import User
 from simple_import.models import *
 from django.core.files import File
@@ -13,12 +13,11 @@ class SimpleTest(TestCase):
         user.is_staff = True
         user.is_superuser = True
         user.save()
-        self.c = Client()
-        self.c.login(username='temporary', password='temporary')
+        self.client.login(username='temporary', password='temporary')
         self.absolute_path = os.path.join(os.path.dirname(__file__), 'static', 'test_import.xls')
         self.import_setting = ImportSetting.objects.create(
             user=user,
-            content_type=ContentType.objects.get(name="import log", app_label="simple_import")
+            content_type=ContentType.objects.get_for_model(ImportLog)
         )
         with open(self.absolute_path) as fp:
             self.import_log = ImportLog.objects.create(
@@ -31,15 +30,21 @@ class SimpleTest(TestCase):
         
     def test_import(self):
         """ Make sure we can upload the file and match columns """
-        import_log_ct_id = ContentType.objects.get(name="import log", app_label="simple_import").id
+        import_log_ct_id = ContentType.objects.get_for_model(ImportLog).id
+        
+        self.assertEqual(ImportLog.objects.count(), 1)
         
         with open(self.absolute_path) as fp:
-            response = self.c.post('/simple_import/start_import/', {
+            response = self.client.post(reverse('simple_import-start_import'), {
                 'name': 'This is a test',
                 'import_file': fp,
                 'import_type': "N",
                 'model': import_log_ct_id}, follow=True)
-        self.assertContains(response, '<h1>Match columns</h1>')
+        
+        self.assertEqual(ImportLog.objects.count(), 2)
+        
+        self.assertRedirects(response, reverse('simple_import-match_columns', kwargs={'import_log_id': ImportLog.objects.all()[1].id}))
+        self.assertContains(response, '<h1>Match Columns</h1>')
         # Check matching
         self.assertContains(response, '<option value="name" selected="selected">')
         self.assertContains(response, '<option value="user" selected="selected">')
@@ -47,37 +52,67 @@ class SimpleTest(TestCase):
         self.assertContains(response, '<option value="import_setting">import setting (Required) (Related)</option>')
         # Check Sample Data
         self.assertContains(response, '/tmp/foo.xls')
+    
+    def test_match_columns(self):
+        """ Test matching columns view  """
+        self.assertEqual(ColumnMatch.objects.count(), 0)
         
+        response = self.client.post(
+            reverse('simple_import-match_columns', kwargs={'import_log_id': self.import_log.id}), {
+            'columnmatch_set-TOTAL_FORMS':6,
+            'columnmatch_set-INITIAL_FORMS':6,
+            'columnmatch_set-MAX_NUM_FORMS':1000,
+            'columnmatch_set-0-id':1,
+            'columnmatch_set-0-column_name':'name',
+            'columnmatch_set-0-import_setting':self.import_setting.id,
+            'columnmatch_set-0-field_name':'name',
+            'columnmatch_set-1-id':2,
+            'columnmatch_set-1-column_name':'UseR',
+            'columnmatch_set-1-import_setting':self.import_setting.id,
+            'columnmatch_set-1-field_name':'user',
+            'columnmatch_set-2-id':3,
+            'columnmatch_set-2-column_name':'nothing',
+            'columnmatch_set-2-import_setting':self.import_setting.id,
+            'columnmatch_set-2-field_name':'',
+            'columnmatch_set-3-id':4,
+            'columnmatch_set-3-column_name':'import file',
+            'columnmatch_set-3-import_setting':self.import_setting.id,
+            'columnmatch_set-3-field_name':'import_file',
+            'columnmatch_set-4-id':5,
+            'columnmatch_set-4-column_name':'import_setting',
+            'columnmatch_set-4-import_setting':self.import_setting.id,
+            'columnmatch_set-4-field_name':'import_setting',
+            'columnmatch_set-5-id':6,
+            'columnmatch_set-5-column_name':'importtype',
+            'columnmatch_set-5-import_setting':self.import_setting.id,
+            'columnmatch_set-5-field_name':'import_type',
+        }, follow=True)
+        
+        self.assertRedirects(response, reverse('simple_import-match_relations', kwargs={'import_log_id': self.import_log.id}))
+        self.assertContains(response, '<h1>Match Relations and Prepare to Run Import</h1>')
+        self.assertEqual(ColumnMatch.objects.count(), 6)
+    
     def test_match_relations(self):
         """ Test matching relations view  """
-        return #IndexError: list index out of range - WHY?
-        response = self.c.post('/simple_import/match_columns/{0}/'.format(self.import_log.id), {
-            'form-TOTAL_FORMS':6,
-            'form-INITIAL_FORMS':6,
-            'form-MAX_NUM_FORMS':1000,
-            'form-0-id':1,
-            'form-0-column_name':'name',
-            'form-0-import_setting':self.import_setting.id,
-            'form-0-field_name':'name',
-            'form-1-id':2,
-            'form-1-column_name':'UseR',
-            'form-1-import_setting':self.import_setting.id,
-            'form-1-field_name':'user',
-            'form-2-id':3,
-            'form-2-column_name':'nothing',
-            'form-2-import_setting':self.import_setting.id,
-            'form-2-field_name':'',
-            'form-3-id':4,
-            'form-3-column_name':'import file',
-            'form-3-import_setting':self.import_setting.id,
-            'form-3-field_name':'import_file',
-            'form-4-id':5,
-            'form-4-column_name':'import_setting',
-            'form-4-import_setting':self.import_setting.id,
-            'form-4-field_name':'import_setting',
-            'form-5-id':6,
-            'form-5-column_name':'importtype',
-            'form-5-import_setting':self.import_setting.id,
-            'form-5-field_name':'import_type',
+        self.assertEqual(RelationalMatch.objects.count(), 0)
+        
+        response = self.client.post(
+            reverse('simple_import-match_relations', kwargs={'import_log_id': self.import_log.id}), {
+            'relationalmatch_set-TOTAL_FORMS':2,
+            'relationalmatch_set-INITIAL_FORMS':2,
+            'relationalmatch_set-MAX_NUM_FORMS':1000,
+            'relationalmatch_set-0-id':1,
+            'relationalmatch_set-0-import_log':self.import_log.id,
+            'relationalmatch_set-0-field_name':'user',
+            'relationalmatch_set-0-related_field_name':'simple_import_log',
+            'relationalmatch_set-1-id':2,
+            'relationalmatch_set-1-import_log':self.import_log.id,
+            'relationalmatch_set-1-field_name':'import_setting',
+            'relationalmatch_set-1-related_field_name':'importlog_set',
         }, follow=True)
-        self.assertContains(response, 'Match Relations and Prepare to Run Import')
+        
+        self.assertRedirects(response, reverse('simple_import-do_import', kwargs={'import_log_id': self.import_log.id}))
+        self.assertContains(response, '<h1>Import Results</h1>')
+        
+        self.assertEqual(RelationalMatch.objects.count(), 2)
+

simple_import/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls.defaults import *
 from simple_import import views
 
 urlpatterns = patterns('',
-    url('^start_import/$', views.start_import),
-    url('^match_columns/(?P<import_log_id>\d+)/$', views.match_columns),
-    url('^match_relations/(?P<import_log_id>\d+)/$', views.match_relations),
-    url('^do_import/(?P<import_log_id>\d+)/$', views.do_import),
+    url('^start_import/$', views.start_import, name='simple_import-start_import'),
+    url('^match_columns/(?P<import_log_id>\d+)/$', views.match_columns, name='simple_import-match_columns'),
+    url('^match_relations/(?P<import_log_id>\d+)/$', views.match_relations, name='simple_import-match_relations'),
+    url('^do_import/(?P<import_log_id>\d+)/$', views.do_import, name='simple_import-do_import'),
 )

commit 8041824173740469eb390c1d01c23b04df6d27b7

In match_columns, only show ColumnMatches for the current file's columns, not every column the user has ever seen. Problem introduced in a9a6b4e087caae42250586c8806e3d5b353aecd7.

simple_import/views.py
@@ -75,7 +75,7 @@ def match_columns(request, import_log_id):
         raise SuspiciousOperation("Non superuser attempting to view other users import")
     
     # need to generate matches if they don't exist already
-    import_log.get_matches()
+    existing_matches = import_log.get_matches()
     
     MatchFormSet = inlineformset_factory(ImportSetting, ColumnMatch, form=MatchForm, extra=0)
     
@@ -129,7 +129,7 @@ def match_columns(request, import_log_id):
                     match_relations,
                     kwargs={'import_log_id': import_log.id}))
     else:
-        formset = MatchFormSet(instance=import_log.import_setting)
+        formset = MatchFormSet(instance=import_log.import_setting, queryset=existing_matches)
     
     field_choices = (('', 'Do Not Use'),)
     for field_name in field_names: