e515714caa72330aaba5ba840a19fd2a6813e2c5
smcoll <smcoll@gmail.com>
2015-06-08 10:32:57 -0500

    Fix for #47

--- simple_import/migrations/0001_initial.py
+++ simple_import/migrations/0001_initial.py
@@ -8,7 +8,7 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('contenttypes', '0002_remove_content_type_name'),
+        ('contenttypes', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
